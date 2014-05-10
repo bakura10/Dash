ae7a00e32dc31805681f64e6c91715125b270c57
Michaël Gallego <mic.gallego@gmail.com>
2014-05-10 22:02:48 +0200

    Add benchmark

---
+++ benchmarks/DashBenchmark/DashBenchmark.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace DashBenchmark;
+
+use Athletic\AthleticEvent;
+use Dash\Module;
+use Zend\Http\Request as HttpRequest;
+use Zend\ServiceManager\Config;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceManager;
+use Zend\ServiceManager\ServiceManagerAwareInterface;
+
+class DashBenchmark extends AthleticEvent
+{
+    protected $serviceManager;
+
+    protected $assemblyRouter;
+
+    public function classSetUp()
+    {
+        $module = new Module();
+        $config = $module->getConfig();
+
+        $serviceManager = new ServiceManager(new Config($config['service_manager']));
+        $serviceManager->addInitializer(function ($instance) use ($serviceManager) {
+            if ($instance instanceof ServiceManagerAwareInterface) {
+                $instance->setServiceManager($serviceManager);
+            }
+        });
+        $serviceManager->addInitializer(function ($instance) use ($serviceManager) {
+            if ($instance instanceof ServiceLocatorAwareInterface) {
+                $instance->setServiceLocator($serviceManager);
+            }
+        });
+        $serviceManager->setService(
+            'Config',
+            [
+                'dash_router' => [
+                    'routes' => [
+                        'home' => ['/', 'foo', 'bar'],
+                        'user' => ['/user', 'foo', 'bar', 'children' => [
+                            'create' => ['/create', 'foo', 'bar'],
+                            'edit' => ['/edit/:id', 'foo', 'bar', 'constraints' => ['id' => '\d+']],
+                            'delete' => ['/delete/:id', 'foo', 'bar', 'constraints' => ['id' => '\d+']],
+                        ]],
+                        'blog' => ['/blog', 'foo', 'bar', 'children' => [
+                            'article' => ['/:slug', 'foo', 'bar'],
+                            'create' => ['/create', 'foo', 'bar'],
+                            'edit' => ['/edit/:id', 'foo', 'bar', 'constraints' => ['id' => '\d+']],
+                            'delete' => ['/delete/:id', 'foo', 'bar', 'constraints' => ['id' => '\d+']],
+                        ]],
+                    ],
+                ],
+            ]
+        );
+
+        $this->serviceManager = $serviceManager;
+
+        // For assembling, we keep the same router around.
+        $this->assemblyRouter = $this->serviceManager->get('Dash\Router\Http\Router');
+    }
+
+    /**
+     * @iterations 1000
+     */
+    public function firstMatch()
+    {
+        $request = new HttpRequest();
+        $request->setUri('http://example.com/blog/delete/1');
+
+        $router = $this->serviceManager->get('Dash\Router\Http\Router');
+        $router->match($request);
+    }
+
+    /**
+     * @iterations 1000
+     */
+    public function lastMatch()
+    {
+        $request = new HttpRequest();
+        $request->setUri('http://example.com/');
+
+        $router = $this->serviceManager->get('Dash\Router\Http\Router');
+        $router->match($request);
+    }
+
+    /**
+     * @iterations 1000
+     */
+    public function assemble()
+    {
+        $this->assemblyRouter->assemble(['id' => 1], ['name' => 'blog/delete']);
+    }
+}
